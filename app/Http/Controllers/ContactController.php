2697d30758e92134d8a0fae19e34cc00ce4f3bb0
Takano Computer <goyataiko@gmail.com>
2023-08-11 01:35:32 +0900

    validation機能設定

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,21 +3,29 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Contact;
+use App\Http\Requests\FormSubmissionRequest;
 
 class ContactController extends Controller
 {
     //
     public function index(){
-        $users = Contact::paginate(3);  //5개 데이터를 2개씩 페이지를 나눠라..
+        // $users = Contact::paginate(3);  //5개 데이터를 2개씩 페이지를 나눠라..
+        $users = Contact::get();
         
         return view('contact.index', compact('users'));
     }
 
-    public function store(Request $request){        
+    public function store(FormSubmissionRequest $request){
+        $this -> validate($request,[
+            'name' => 'required|min:2',
+            'gender' => 'required'
+        ]); 
+
         Contact::create([
             'name' => $request -> get('name'),
             'gender' => $request -> get('gender')
         ]);
-        return redirect('/contacts');
+
+        return redirect('/contacts')->with('success','컨텍트가 작성됨');
     }
 }
--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -1,10 +1,31 @@
+<style>
+    .error {
+        color: red;
+        font-weight: bold;
+    }
+
+    .success {
+        color: green;
+        font-weight: bold;
+    }
+</style>
+
 <h1>List of users</h1>
+@if(Session::has('success'))
+    <p class="success">{{Session::get('success')}}</p>
+@endif
+
+@if($errors->has('name'))
+    <p class="error">{{$errors->first('name')}}</p>
+@endif
+@if($errors->has('gender'))
+    <p class="error">{{$errors->first('gender')}}</p>
+@endif
 
 @if(count($users)>0)
     @foreach ($users as $user)
     <p>Name: {{$user->name}} | Gender: {{$user->gender}}</p>
     @endforeach
-    {{!! $users->links() !!}}
 @else
 <p>No Record</p>    
 @endif
